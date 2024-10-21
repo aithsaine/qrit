b7767ea0a735cb6fc482f7bf0d7eea4a9c01fc06
ismail ait hsaine <aithsaine3@gmail.com>
2024-10-21 13:48:39 +0100

    add table

--- server/database/migrations/2024_10_21_111035_create_tables_table.php
+++ server/database/migrations/2024_10_21_111035_create_tables_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('tables', function (Blueprint $table) {
             $table->id();
-            $table->integer('num_table');
+            $table->integer('num_table')->unique();
             $table->timestamps();
         });
     }

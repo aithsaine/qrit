58c1e27c0493a5a748c94bd030ec0bdc875e8f0a
swi3a <saadcham97@gmail.com>
2024-10-28 12:12:26 +0100

    new changes

--- client/src/views/admin/employee/index.jsx
+++ client/src/views/admin/employee/index.jsx
@@ -1,22 +1,23 @@
-import NewTableModal from "components/table/addTable";
-import NewEmployeeModal from "../../../components/employee/addEmployee";
-import React from "react";
-import { useSelector } from "react-redux";
-import EmployeCard from "components/card/EmployeCard";
+import NewTableModal from 'components/table/addTable'
+import NewEmployeeModal from '../../../components/employee/addEmployee'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import EmployeCard from 'components/card/EmployeCard'
+import List from './table/List'
+
 
 export default function Employee() {
-  const employees = useSelector((state) => state.employees);
-  return (
-    <div className="mt-4 flex w-full flex-col">
-      <div className="my-4 grid grid-cols-1 gap-4 md:grid-cols-3">
-        <NewEmployeeModal />
-        <NewTableModal />
-      </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-      {employees.map((emp) => (
-          <EmployeCard employee={emp} />
-        ))}
-      </div>
-    </div>
-  );
+    const employees = useSelector(state=>state.employees)
+    return (
+        <div className='flex w-full mt-4 flex-col'>
+            <div className='flex space-x-2 my-4'>
+                <NewEmployeeModal/>
+                <NewTableModal/>
+            </div>
+            <List />
+            {/* <div className='grid grid-cols-3 gap-2'> 
+                {employees.map(emp=><EmployeCard employee={emp}/>)}
+            </div> */}
+        </div>
+    )
 }

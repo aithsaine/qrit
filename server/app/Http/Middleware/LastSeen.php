9d6b6c3b9b0129a1b49f9e753d5b36033f1b77db
ismail ait hsaine <aithsaine3@gmail.com>
2024-11-13 21:57:17 +0100

    live employees status back

--- server/app/Http/Middleware/LastSeen.php
+++ server/app/Http/Middleware/LastSeen.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Middleware;
 
+use App\Events\EmployeStatusUpdate;
+use App\Http\Resources\EmployeeResource;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +22,11 @@ class LastSeen
             $user = $request->user();
             $user->last_seen = now();
             $user->save();
+            if($user->employee)
+            {
+
+                event(new EmployeStatusUpdate(new EmployeeResource($user->employee)));
+            }
             // broadcast(new UpdateUserStatus($user));
         }
         return $next($request);
